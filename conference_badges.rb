6868fba864d3ec8c52b79aefc36f7014dbf32c7e
Denise Pendleton <dpendl1@hotmail.com>
2018-02-20 16:10:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,8 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(arr)
-  # arr = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus",  "Matz"]
+  arr = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus",  "Matz"]
   new_arr = []
-    new_array << each {|name| puts "Hello, my name is #{name}."}
-    new_array
+   each {|name| puts "Hello, my name is #{name}."}
+    new_array << name
 end
