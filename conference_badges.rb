794ba75fc92a4b52e90f19b1f74906be5134476e
Denise Pendleton <dpendl1@hotmail.com>
2018-02-20 16:58:47 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,5 +14,6 @@ def assign_rooms(speakers)
   new_arr = []
   speakers.each_with_index do |name, i|
     new_arr <<  "Hello, #{name}! You'll be assigned to room #{i + 1}!"
+    new_arr
   end
   end
